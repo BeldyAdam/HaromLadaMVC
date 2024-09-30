a8f274e75df670c0d569fa82c12588c10717617e
BeldyAdam <beldy.adam@gmail.com>
2024-09-30 16:04:36 +0200

    MVC Felugro ablakos nézet

--- src/main/KincsesLada.java
+++ src/main/KincsesLada.java
@@ -1,14 +1,24 @@
 package main;
 
-import model.Jatek;
+import controller.FelugroVezerlo;
+import controller.KonzolVezerlo;
+import model.JatekModel;
+import view.FelugroNezet;
+import view.KonzolNezet;
 
 
 public class KincsesLada {
 
 
     public static void main(String[] args) {
-//        System.out.println(new Jatek().kezdes());
-        new Jatek();
+//        System.out.println(new JatekModel().kezdes());
+//        new JatekModel();
+        JatekModel model = new JatekModel();
+        KonzolNezet nezet = new KonzolNezet();
+//        new KonzolVezerlo(model, nezet);
+        
+        FelugroNezet fNezet = new FelugroNezet();
+        new FelugroVezerlo(model, fNezet);
         
     }
     
