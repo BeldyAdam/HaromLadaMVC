aa20037b668736bd34c5dafd423a7ef1182d68b1
BeldyAdam <beldy.adam@gmail.com>
2024-09-30 14:53:43 +0200

    oop konzolos megjelenites

---
+++ src/main/KincsesLada.java
@@ -0,0 +1,15 @@
+package main;
+
+import model.Jatek;
+
+
+public class KincsesLada {
+
+
+    public static void main(String[] args) {
+//        System.out.println(new Jatek().kezdes());
+        new Jatek();
+        
+    }
+    
+}
